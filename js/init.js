c08ee896dc3cf8b9a90850f17b712e44dfeda6cf
MarieCocci <tibbphoque@gmail.com>
2016-09-27 20:51:31 +0200

    firts add
    add : mobile menu
    + choose language
    + categories principle

--- js/init.js
+++ js/init.js
@@ -1,8 +1,30 @@
-(function($){
-  $(function(){
+$(function(){
+	$('.button-collapse').sideNav();
+	$('.parallax').parallax();
+	$('select').material_select();
+	DOC_CONFIG={}
+	//$.getJSON("site_test.json",function(data){
+	//	DOC_CONFIG = data
+	//	generateDocSite(DOC_CONFIG);
+	//});
+	DOC_CONFIG = jQuery.parseJSON('{"categories":{"main":{"name":{"fr_FR":"Général","en_US":"Main"},"icon":"","docs":[{"name":"Installation","link":"installation/#language#/index.html"},{"name":"Compatibilité","link":"compatibility/#language#/index.html"}]},"howto":{"name":{"fr_FR":"Tutoriaux","en_US":"How to"},"icon":""},"plugins":{"name":{"fr_FR":"Plugins","en_US":"Plugins"},"icon":""}}}')
+	generateDocSite(DOC_CONFIG);
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+	$('#sel_language').on('change',function(){
+		generateDocSite(DOC_CONFIG);
+	})
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+});
+
+
+
+function generateDocSite(data){
+	$('#ul_listCategory').empty();
+	console.log(data);
+	for(var i in data.categories){
+		$('#ul_listCategory').append('<li class="collection-item">'+data.categories[i].name[$('#sel_language').val()]+'</li>');
+
+		
+	//	console.log(data.categories[i])
+}
+}
