b8b6d6f00a8ae83cb3b0a33d1496d12dc10ca54c
MarieCocci <tibbphoque@gmail.com>
2016-10-06 19:44:34 +0200

    add scroll + waves effect

--- js/init.js
+++ js/init.js
@@ -2,21 +2,26 @@ $(function(){
 	$('.button-collapse').sideNav();
 	$('.parallax').parallax();
 	$('select').material_select();
-	DOC_CONFIG={}
-	$.getJSON("site.json",function(data){
-		DOC_CONFIG = data
-		generateCatSite(DOC_CONFIG);
-	});
-	//DOC_CONFIG = jQuery.parseJSON('{"main":{"name":{"fr_FR":"Général","en_US":"Main"},"icon":"","docs":[{"name":"installation","url":"installation/#language#/index.html"},{"name":"Compatibilité","url":"compatibility/#language#/index.html"}]},"howto":{"name":{"fr_FR":"Tutoriaux","en_US":"How to"},"icon":"","docs":[{"name":"Howto1","url":"installation/#language#/index.html"},{"name":"Howto2","url":"compatibility/#language#/index.html"}]},"plugins":{"name":{"fr_FR":"Plugins","en_US":"Plugins"},"icon":"","docs":[{"name":"plugin1","url":"installation/#language#/index.html"},{"name":"PLUGIN2","url":"compatibility/#language#/index.html"},{"name":"pLuGiN tOtO3","url":"compatibility/#language#/index.html"}]}}')
-	//generateCatSite(DOC_CONFIG);
 
+	//use='local';
+	use='online';
+	DOC_CONFIG={}
 	THIRD_CONFIG={}
-	$.getJSON("site_third.json",function(data){
-		THIRD_CONFIG = data
+	if (use=='online'){
+		$.getJSON("site.json",function(data){
+			DOC_CONFIG = data
+			generateCatSite(DOC_CONFIG);
+		});
+		$.getJSON("site_third.json",function(data){
+			THIRD_CONFIG = data
+			generateThirdSite(THIRD_CONFIG);
+		});
+	}else{
+		DOC_CONFIG = jQuery.parseJSON('{"main":{"name":{"fr_FR":"Général","en_US":"Main"},"icon":"","docs":[{"name":"installation","url":"installation/#language#/index.html"},{"name":"Compatibilité","url":"compatibility/#language#/index.html"}]},"howto":{"name":{"fr_FR":"Tutoriaux","en_US":"How to"},"icon":"","docs":[{"name":"Howto1","url":"installation/#language#/index.html"},{"name":"Howto2","url":"compatibility/#language#/index.html"}]},"plugins":{"name":{"fr_FR":"Plugins","en_US":"Plugins"},"icon":"","docs":[{"name":"plugin1","url":"installation/#language#/index.html"},{"name":"PLUGIN2","url":"compatibility/#language#/index.html"},{"name":"pLuGiN tOtO3","url":"compatibility/#language#/index.html"}]}}')
+		generateCatSite(DOC_CONFIG);
+		THIRD_CONFIG = jQuery.parseJSON('{"third_plugin":{"docs":[{"url":"third_plugin/ecowatt/#language#/index.html","name":"Eco-2-watt"},{"url":"third_plugin/ecowatt/#language#/index.html","name":"Test1"},{"url":"third_plugin/ecowatt/#language#/index.html","name":"Test2"},{"url":"third_plugin/ecowatt/#language#/index.html","name":"Test3"}],"name":{"en_US":"Thierd plugins","fr_FR":"Plugins tierces"}}}')
 		generateThirdSite(THIRD_CONFIG);
-	});
-	//THIRD_CONFIG = jQuery.parseJSON('{"third_plugin":{"docs":[{"url":"third_plugin/ecowatt/#language#/index.html","name":"Eco-2-watt"},{"url":"third_plugin/ecowatt/#language#/index.html","name":"Test1"},{"url":"third_plugin/ecowatt/#language#/index.html","name":"Test2"},{"url":"third_plugin/ecowatt/#language#/index.html","name":"Test3"}],"name":{"en_US":"Thierd plugins","fr_FR":"Plugins tierces"}}}')
-	//generateThirdSite(THIRD_CONFIG);
+	}
 
 	$('#sel_language').on('change',function(){
 		generateCatSite(DOC_CONFIG);
@@ -37,14 +42,14 @@ function generateCatSite(data){
 	$('#ul_listCategory').empty();
 	$('#ul_listDoc').empty();
 	for(var i in data){
-		$('#ul_listCategory').append('<a class="collection-item" data-key="'+i+'">'+convertCase(data[i].name[$('#sel_language').val()])+'<span class="badge">'+data[i].docs.length+'</span>'+'</a>');
+		$('#ul_listCategory').append('<a class="collection-item waves-effect waves-jeedom" data-key="'+i+'">'+convertCase(data[i].name[$('#sel_language').val()])+'<span class="badge">'+data[i].docs.length+'</span>'+'</a>');
 	}
 }
 
 function generateDocSite(data){
 	$('#ul_listDoc').empty();
 	for(var i in data.docs){
-		$('#ul_listDoc').append('<a class="collection-item" target="_blank" href="'+data.docs[i].url.replace('#language#',$('#sel_language').val())+'">'+convertCase(data.docs[i].name)+'</a>');
+		$('#ul_listDoc').append('<a class="collection-item waves-effect waves-jeedom" target="_blank" href="'+data.docs[i].url.replace('#language#',$('#sel_language').val())+'">'+convertCase(data.docs[i].name)+'</a>');
 	}
 }
 
@@ -54,6 +59,6 @@ function generateThirdSite(data){
 	$('#ul_listThird2').empty();
 	for(var i in data.third_plugin.docs){
 		colNb=i % 3;
-		$('#ul_listThird'+colNb).append('<a class="collection-item" target="_blank" href="'+data.third_plugin.docs[i].url.replace('#language#',$('#sel_language').val())+'">'+convertCase(data.third_plugin.docs[i].name)+'</a>');
+		$('#ul_listThird'+colNb).append('<a class="collection-item waves-effect waves-jeedom" target="_blank" href="'+data.third_plugin.docs[i].url.replace('#language#',$('#sel_language').val())+'">'+convertCase(data.third_plugin.docs[i].name)+'</a>');
 	}
 }
